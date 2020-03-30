fe0e044e2f051bc320e26622bde3e6f98357a575
DouglasFuelber <dffuelber@gmail.com>
2020-03-30 17:50:21 -0300

    Fixed switch statement

--- CleanCode/SwitchStatements/Customer.cs
+++ CleanCode/SwitchStatements/Customer.cs
@@ -1,14 +1,7 @@
-﻿
-namespace CleanCode.SwitchStatements
+﻿namespace CleanCode.SwitchStatements
 {
-    public class Customer
+    public abstract class Customer
     {
-        public CustomerType Type { get; set; }
-    }
-
-    public enum CustomerType
-    {
-        PayAsYouGo = 1,
-        Unlimited
+        public abstract MonthlyStatement GenerateStatement(MonthlyUsage monthlyUsage);
     }
 }
